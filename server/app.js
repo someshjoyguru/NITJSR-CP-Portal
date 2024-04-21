a5cb1fa9b4ba0ea607f37a1aa22bbf1bd22a3e94
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-04-22 03:06:54 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173','https://achieveit-mocha.vercel.app', 'https://achieve-it-omega.vercel.app'],
+    origin: ['http://localhost:5173,'https://nitjsr-cp-portal.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
