10f28d018726dc6f36633b531b0de5bf26318daa
Somesh Ghosh <someshghoshjoyguru@gmail.com>
2024-05-18 10:44:34 +0530

    shanti

--- server/routes/leaderboard.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { isAuthenticated } from "../middlewares/auth.js";
-import { showLeaderboard } from "../controllers/leaderboard.js";
-
-const router = express.Router();
-
-router.get("/", isAuthenticated, showLeaderboard);
-export default router;
